1df7caf46669af4bf6da59e2f6ff57596ccc8bb5
Jiri Stepan <jiri.stepan@activate.cz>
2018-10-29 11:23:36 +0100

    drobne zmeny

--- index.js
+++ index.js
@@ -13,24 +13,31 @@ class Mailkit{
  
     }
 
+    /*
+    * Odeslani emailu na jednoho uzivatele. Predpoklada jako parametr struktutu
+    * {mailingListId, campaignId, email, subject, messageData, content: {}, 
+    *  user:{ vocative, lastName, firstName }}
+    *  
+    * https://www.mailkit.eu/cz/napoveda-pomoc/dokumentace/api/dorucovani-kampane/mailkitsendmail/
+    * */
     sendmail(par){
         let params = []
         params.push(this.clientId)
         params.push(this.clientMD5)
-        params.push(par.mailingListId)  //kde vezmene
-        params.push(par.campaignId)     //kde vezmem
+        params.push(par.mailingListId)  //kde vezmeme?
+        params.push(par.campaignId)     //kde vezmeme?
         params.push({
             "send_to" : par.email,
             "subject" : par.subject,
-            "message_data" : par.messageData,
+            "message_data" : par.messageData, // co to sakra je?
             "content" : {}, // tady mohou byt promene. Asi název akce??
 
         })
         params.push({
-            "vocative": par.vocative,
-            "prefix": par.prefix,
-            "last_name": par.lastName,
-            "first_name": par.firstName
+            "vocative": par.user.vocative, //to se sem musí posílat?
+            "prefix": par.user.prefix,    
+            "last_name": par.user.lastName,
+            "first_name": par.user.firstName
         })
 
         xmlrpcClient.methodCall('sendmail', params, function (error, value) {
