11b9bd2da52e29acc7d8cec94f7c27869e65ec3b
Jiri Stepan <jiri.stepan@activate.cz>
2018-10-29 07:46:18 +0100

    test verze

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+class Mailkit{
+    constructor(){
+        this.version = "0.1.0"
+    }
+
+    getVersion(){
+        return "Mailkik ["+this.version+"]"
+    }
+}
+
+module.exports = Mailkit
---
+++ test.js
@@ -0,0 +1,4 @@
+const Mailkit = require("./index.js")
+
+let M = new Mailkit()
+console.log(M.getVersion())
